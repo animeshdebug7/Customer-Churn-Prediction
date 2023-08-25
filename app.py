6a3324a85fa8fb1478fa6c1643b1bfd982f97c5c
Animesh Singh <66486050+animeshdebug7@users.noreply.github.com>
2023-08-25 13:08:39 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -16,17 +16,19 @@ def home():
 def predict():
     if request.method == "POST":
        data = []
+       lst = []
        Age = request.form.get("Age")
        Subscription_Length_Months = request.form.get("Subscription_Length_Months")
        Monthly_Bill = request.form.get("Monthly_Bill")
        Total_Usage_GB = request.form.get("Total_Usage_GB")
-       data.append(Age)
-       data.append(Subscription_Length_Months)
-       data.append(Monthly_Bill)
-       data.append(Total_Usage_GB)
-       data = list(data)
-       model.predict(data)
-    return render_template('result.html', result = data)
+       data.append(int(Age))
+       data.append(int(Subscription_Length_Months))
+       data.append(int(Monthly_Bill))
+       data.append(int(Total_Usage_GB))
+       lst.append(data)
+       print(lst)
+       res = model.predict(lst)
+    return render_template('result.html', result = res)
 
 if __name__ == '__main__':  
     app.run(debug = True)
